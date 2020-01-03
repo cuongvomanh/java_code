3009442df5163e153bfef3b8be458ce2d7d7e4b5
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2020-01-03 11:24:38 +0700

    Update Something.java

--- Something.java
+++ Something.java
@@ -8,3 +8,30 @@ Gw gw83 = new Gw("83", ipPrefix, "web_old", gwNo);
 Gw gw84 = new Gw("84", ipPrefix, "web_old", gwNo);
 Set<Gw> gwSet = new HashSet<>();
 gwSet = Stream.of(gw66, gw67, gw68, gw82, gw83, gw84).collect(Collectors.toSet());
+
+
+
+/////////////////////////////////////////////////////////////////
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class SetUtil
+{
+    // Program to filter set in Java
+    public static void main(String[] args)
+    {   
+        Set<String> set = new HashSet<>(
+                                Arrays.asList("C", "C++", "Java", "C#"));
+
+        // Java 8 and above:
+
+        Set<String> filteredSet = set.stream()
+                                    .filter(s -> s.startsWith("C"))
+                                    .collect(Collectors.toSet());
+
+        System.out.println(filteredSet);
+    }   
+}
+
