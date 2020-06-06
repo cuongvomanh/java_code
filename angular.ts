97d56f4f62cce03fed6bd8067e8598c5f2f5d6ee
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2020-06-06 14:54:48 +0700

    Create angular.ts

---
+++ angular.ts
@@ -0,0 +1,11 @@
+for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+      const begin = sliceIndex * sliceSize;
+      const end = Math.min(begin + sliceSize, bytesLength);
+
+      const bytes = new Array(end - begin);
+      for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+        bytes[i] = byteCharacters[offset].charCodeAt(0);
+      }
+      byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+    return new Blob(byteArrays);
